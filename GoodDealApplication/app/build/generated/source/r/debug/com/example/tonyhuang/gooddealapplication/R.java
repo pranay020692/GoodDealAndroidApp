f315d232d5d72ae598a5f370251d635b234158de
pranay020692 <pranay020692@gmail.com>
2015-12-05 15:39:00 -0500

    Theming UI

--- GoodDealApplication/app/build/generated/source/r/debug/com/example/tonyhuang/gooddealapplication/R.java
+++ GoodDealApplication/app/build/generated/source/r/debug/com/example/tonyhuang/gooddealapplication/R.java
@@ -1569,7 +1569,9 @@ containing a value of this type.
         public static final int background_main=0x7f02003e;
         public static final int button_comp=0x7f02003f;
         public static final int button_scan=0x7f020040;
-        public static final int notification_template_icon_bg=0x7f020041;
+        public static final int comp=0x7f020041;
+        public static final int notification_template_icon_bg=0x7f020043;
+        public static final int scan=0x7f020042;
     }
     public static final class id {
         public static final int action0=0x7f0c005a;
